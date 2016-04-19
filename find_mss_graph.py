9057b619433d8c0e131fb44523e092c7661f7adb
Aaron Sherman <ajs@ajs.com>
2016-04-19 12:48:35 -0400

    Added more options and better status

--- find_mss_graph.py
+++ find_mss_graph.py
@@ -16,7 +16,22 @@ def arg_parser():
     parser.add_argument(
         '-v', '--verbose',
         action='store_true')
+    parser.add_argument(
+        '--skip-short-entries', action='store_true',
+        default=False,
+        help="Skip many possible solutions, only testing the most promising")
     return parser
+
+def longrange(n):
+    """Similar to xrange(n), but roll over to longs as needed"""
+
+    i = 0
+    while True:
+        if i >= n:
+            break
+        else:
+            yield i
+        i += 1
 
 
 class MSSGraphFinder(mss_finder.SquareSquareFinder):
@@ -25,36 +40,48 @@ class MSSGraphFinder(mss_finder.SquareSquareFinder):
     properties of the square.
     """
 
-    def __init__(self):
+    def __init__(self, skip_short_entries=False, verbose=False):
+        self.skip_short_entries = skip_short_entries
+        self.verbose = verbose
         self.cur_score = 0
 
     def find(self):
         def map_up(n):
             return (n+1)*(n+1)
+
+        def show_sums(i, j, k, count, entry):
+            print "%d+%d+%d = %d (%d matches): %r" % (
+                k, j, i, k+j+i, count, entry)
+
+        cur_max_sums = 0
         for i in itertools.count(0,1):
             s = map_up(i)
             #print i, s
             sums = {}
-            j = -1
-            while True:
-                j += 1
-                if j == i:
-                    break
+            for j,k in itertools.combinations(longrange(i), 2):
+                k,j = (min((j,k)), max((j,k)))
                 sj = map_up(j)
-                k = -1
-                while True:
-                    k += 1
-                    if k == j:
-                        break
-                    sk = map_up(k)
-                    ssum = str(sj + sk + s)
-                    if not ssum in sums:
-                        sums[ssum] = [(sj, sk, s)]
-                    else:
-                        sums[ssum].append((sj,sk,s))
-                        if len(sums[ssum]) > self.cur_score:
-                            if self.try_graph(sums[ssum], i, s):
-                                return
+                sk = map_up(k)
+                ssum = str(sj + sk + s)
+                if not ssum in sums:
+                    sums[ssum] = [(sk, sj, s)]
+                    if cur_max_sums == 0:
+                        show_sums(s, sj, sk, 1, sums[ssum])
+                        cur_max_sums = 1
+                else:
+                    entry = sums[ssum]
+                    entry.append((sk,sj,s))
+                    count = len(entry)
+                    if count > self.cur_score:
+                        if count > cur_max_sums:
+                            show_sums(s, sj, sk, count, entry)
+                            cur_max_sums = count
+                        elif self.skip_short_entries and count < cur_max_sums:
+                            continue
+                        elif self.verbose and count == cur_max_sums:
+                            show_sums(s, sj, sk, count, entry)
+                        if self.try_graph(entry, i, s):
+                            return
 
     def is_magic(self, square, **kwargs):
         msquare = self.square_square(square)
@@ -110,7 +137,7 @@ class MSSGraphFinder(mss_finder.SquareSquareFinder):
 def main():
     opts = arg_parser().parse_args()
 
-    gf = MSSGraphFinder()
+    gf = MSSGraphFinder(skip_short_entries=opts.skip_short_entries, verbose=opts.verbose)
     gf.find()
 
 if __name__ == '__main__':
