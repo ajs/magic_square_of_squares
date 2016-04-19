9b3b29256ac3d8a8b113b1d4c3da86e80b29a496
Aaron Sherman <ajs@ajs.com>
2016-04-19 11:01:06 -0400

    A new work in progrees: search via the graph of sums

---
+++ find_mss_graph.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+
+import sys
+import time
+import math
+import random
+import argparse
+import itertools
+
+import mss_finder
+
+
+def arg_parser():
+    parser = argparse.ArgumentParser(
+        description="Find a magic square of cubes")
+    parser.add_argument(
+        '-v', '--verbose',
+        action='store_true')
+    return parser
+
+
+class MSSGraphFinder(mss_finder.SquareSquareFinder):
+    """
+    Find Magic Square of Squares based on the graph
+    properties of the square.
+    """
+
+    def __init__(self):
+        self.cur_score = 0
+
+    def find(self):
+        def map_up(n):
+            return (n+1)*(n+1)
+        for i in itertools.count(0,1):
+            s = map_up(i)
+            #print i, s
+            sums = {}
+            j = -1
+            while True:
+                j += 1
+                if j == i:
+                    break
+                sj = map_up(j)
+                k = -1
+                while True:
+                    k += 1
+                    if k == j:
+                        break
+                    sk = map_up(k)
+                    ssum = str(sj + sk + s)
+                    if not ssum in sums:
+                        sums[ssum] = [(sj, sk, s)]
+                    else:
+                        sums[ssum].append((sj,sk,s))
+                        if len(sums[ssum]) > self.cur_score:
+                            if self.try_graph(sums[ssum], i, s):
+                                return
+
+    def is_magic(self, square, **kwargs):
+        msquare = self.square_square(square)
+        rows = [sum(msquare[r*3+c] for c in range(3)) for r in range(3)]
+        cols = [sum(msquare[r*3+c] for r in range(3)) for c in range(3)]
+        dia1 = sum(msquare[r*3+c] for r,c in ((0,0), (1,1), (2,2)))
+        dia2 = sum(msquare[r*3+c] for r,c in ((0,2), (1,1), (2,0)))
+        all_sums = rows+cols+[dia1, dia2]
+        count = {k:0 for k in all_sums}
+        for magic in all_sums:
+            count[magic] += 1
+        score = max(count.values())
+        if score > self.cur_score:
+            print "Candidate is new top-score with %d matches" % score
+            self.print_sq(square)
+            self.cur_score = score
+        return(score == 8)
+
+    def try_graph(self, start, count, vertex):
+        def map_down(s):
+            return [-1 if s == 0 else int(math.sqrt(ss))-1 for ss in s]
+
+        self.max_value_width = 6
+        possible = set(reduce(lambda x,y: x+y, [list(x) for x in start]))
+        for sset in itertools.combinations(start, 3):
+            if any(vertex not in s for s in sset):
+                print "Failed to find %d in %r" % (vertex, sset)
+                continue
+            # Try each possible diagonal (not all perms because of symetry)
+            for arrangement in ((0,1,2), (0,2,1), (2,1,0)):
+                row = [i for i in sset[arrangement[0]] if i != vertex]
+                col = [i for i in sset[arrangement[1]] if i != vertex]
+                dia = [i for i in sset[arrangement[2]] if i != vertex]
+                for r in itertools.permutations(row):
+                    for c in itertools.permutations(col):
+                        for d in itertools.permutations(dia):
+                            square = [vertex, r[0], r[1], c[0], d[0], 0, c[1], 0, d[1]]
+                            all = set(square)
+                            rest = map_down(possible - all)
+                            square = map_down(square)
+                            #print "Trying:"
+                            #self.print_sq(square)
+                            for s1,s2 in itertools.permutations(rest, 2):
+                                new_s = square[:]
+                                new_s[5] = s1
+                                new_s[7] = s2
+                                if self.is_magic(new_s, quiet=False):
+                                    self.print_sq(new_s)
+                                    return True
+        return False
+
+
+def main():
+    opts = arg_parser().parse_args()
+
+    gf = MSSGraphFinder()
+    gf.find()
+
+if __name__ == '__main__':
+    main()
